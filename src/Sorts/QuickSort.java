322176a35908ad7d57c9940b0900b44f0ef502fe
yogi1062 <yogipowar1062@gmail.com>
2026-04-08 21:54:07 +0530

    Added QuickSort

---
+++ src/Sorts/QuickSort.java
@@ -0,0 +1,49 @@
+package Sorts;
+
+public class QuickSort {
+    static void main(String[] args) {
+        int arr[] = {5,9,1,6,3,2};
+
+        quickSort(arr,0,arr.length-1);
+
+        System.out.println("After sorting ");
+        for (int n:arr){
+            System.out.print(n+" ");
+        }
+
+    }
+
+    private static void quickSort(int[] arr, int low, int high) {
+
+
+        if(low<high){
+
+            int pi = partition(arr,low,high);
+
+            quickSort(arr,low, pi -1);
+            quickSort(arr, pi +1,high);
+        }
+    }
+
+    private static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = low-1;
+
+        for (int j = low; j <high; j++){
+
+            if(arr[j] < pivot){
+
+                i++;
+                int temp = arr[i];
+                arr[i] =arr[j];
+                arr[j]=temp;
+            }
+
+        }
+        int temp = arr[i+1];
+        arr[i+1]=arr[high];
+        arr[high] = temp;
+
+        return i+1;
+    }
+}
